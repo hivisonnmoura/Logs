3c7c5244a1cf1237c83c9958bbefe931ab2ad1a3
Leonardo Alves Chaves <leoalves.chaves@gmail.com>
2016-11-06 15:06:40 -0200

    Add files via upload
    Este commit tem como funcionalidade atualizar a classe RepositorioProcesso

--- AnaliseDeLog/src/main/java/repositorios/RepositorioProcesso.java
+++ AnaliseDeLog/src/main/java/repositorios/RepositorioProcesso.java
@@ -1,5 +1,38 @@
 package repositorios;
 
-public class RepositorioProcesso {
+import java.util.ArrayList;
+
+import entidades.EntidadeProcesso;
+
+public class RepositorioProcesso implements RepositorioGenerico<EntidadeProcesso> {
+	ArrayList<EntidadeProcesso> processo = new ArrayList<EntidadeProcesso>();
+
+	@Override
+	public ArrayList<EntidadeProcesso> select(EntidadeProcesso processo) {
+		return select(processo);
+	}
+
+	@Override
+	public void insert(EntidadeProcesso processo) {
+		this.processo.add(processo);
+
+	}
+
+	@Override
+	public ArrayList<EntidadeProcesso> delete() {
+		return null;
+	}
+
+	@Override
+	public ArrayList<EntidadeProcesso> findall() {
+		
+		return this.processo;
+	}
+
+	@Override
+	public EntidadeProcesso find(EntidadeProcesso obj) {
+
+		return null;
+	}
 
 }
