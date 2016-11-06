9f29785e633ba036ecc469156d5c764b3122188d
Leonardo Alves Chaves <leoalves.chaves@gmail.com>
2016-11-06 15:03:56 -0200

    Add files via upload
    Este commit tem como intenção atualizar a classe FabricaProcesso

--- AnaliseDeLog/src/main/java/fabricas/FabricaProcesso.java
+++ AnaliseDeLog/src/main/java/fabricas/FabricaProcesso.java
@@ -1,5 +1,14 @@
 package fabricas;
 
-public class FabricaProcesso {
+import entidades.EntidadeProcesso;
 
+public class FabricaProcesso {
+	public FabricaProcesso(){
+		
+	}
+	public EntidadeProcesso CriarProcesso(int pid, String username, String time, String cpu, int nlwp, String process){
+		
+		return EntidadeProcesso.CriarProcesso(pid, username, time, cpu, nlwp, process);
+		
+	}
 }
