120f3e29dad1850628c27b299b04d6499771fdfe
Leonardo Alves Chaves <leoalves.chaves@gmail.com>
2016-11-06 15:23:48 -0200

    Update EntidadeThread.java
    este commit tem como função atualizar a classe EntidadeThread.java

--- AnaliseDeLog/src/main/java/entidades/EntidadeThread.java
+++ AnaliseDeLog/src/main/java/entidades/EntidadeThread.java
@@ -1,5 +1,59 @@
 package entidades;
 
 public class EntidadeThread {
+	private int pid;
+	private String username;
+	private String cpu;
+	private String process;
+	private int lwpid;
+
+	public EntidadeThread(int pid, String username, String cpu, String process, int lwpid) {
+		this.pid = pid;
+		this.username = username;
+		this.cpu = cpu;
+		this.process = process;
+		this.lwpid = lwpid;
+	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getCpu() {
+		return cpu;
+	}
+
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+
+	public String getProcess() {
+		return process;
+	}
+
+	public void setProcess(String process) {
+		this.process = process;
+	}
+
+	public int getLwpid() {
+		return lwpid;
+	}
+
+	public void setLwpid(int lwpid) {
+		this.lwpid = lwpid;
+	}
 
 }
+
