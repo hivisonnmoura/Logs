564d0d939e03474ac790a20ce62be5a068d26529
Leonardo Alves Chaves <leoalves.chaves@gmail.com>
2016-11-06 15:30:22 -0200

    Update EntidadeStack.java
    este commit tem a função de atualizar a classe EntidadeStack

--- AnaliseDeLog/src/main/java/entidades/EntidadeStack.java
+++ AnaliseDeLog/src/main/java/entidades/EntidadeStack.java
@@ -1,5 +1,32 @@
 package entidades;
 
 public class EntidadeStack {
+	private String pidHexa;
+	private String stack;
+	
+	public EntidadeStack(String pidHexa, String stack) {
+		this.pidHexa = pidHexa;
+		this.stack = stack;
+	}
 
+	public String getPidHexa() {
+		return pidHexa;
+	}
+
+	public void setPidHexa(String pidHexa) {
+		this.pidHexa = pidHexa;
+	}
+
+	public String getStack() {
+		return stack;
+	}
+
+	public void setStack(String stack) {
+		this.stack = stack;
+	}
+	
+	
+	
+	
 }
+
