6318e090b9db2a47c99041c4f80d648ee1a5cb40
Hivison do Nascimento Moura <hivison.moura@gmail.com>
2016-11-07 15:49:23 -0200

    Merge branch 'Hivison' of https://github.com/wenzaca/AnaliseDeLog into Matheus
    * 'Hivison' of https://github.com/wenzaca/AnaliseDeLog:
      Fix Merge branch master into Matheus
    
    # Conflicts:
    #	AnaliseDeLog/target/classes/META-INF/maven/AnalisedeLog/Indra/pom.properties
    #	AnaliseDeLog/target/classes/entidades/EntidadeProcesso.class
    #	AnaliseDeLog/target/classes/entidades/EntidadeStack.class
    #	AnaliseDeLog/target/classes/entidades/EntidadeThread.class
    #	AnaliseDeLog/target/classes/fabricas/FabricaProcesso.class

--- AnaliseDeLog/src/main/java/interfaces/FrmDiretorio.java
+++ AnaliseDeLog/src/main/java/interfaces/FrmDiretorio.java
@@ -1,29 +1,30 @@
 package interfaces;
 
-import java.awt.BorderLayout;
+import javax.swing.JFileChooser;
 import java.awt.EventQueue;
-import java.awt.Frame;
-
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JTextField;
-import java.awt.FlowLayout;
-import javax.swing.BoxLayout;
-import java.awt.GridLayout;
-import java.awt.CardLayout;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.Box;
-import javax.swing.JSeparator;
 import javax.swing.border.TitledBorder;
+
+
+import servicos.ServicoDescompactador;
+
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FrmDiretorio extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtInserirDiretrio;
 	private JPanel panel;
@@ -50,28 +51,62 @@ public class FrmDiretorio extends JFrame {
 	public FrmDiretorio() {
 		setTitle("Logz");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(500,300, 350, 300);
+		setBounds(500, 300, 350, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Selecionar o Diret�rio", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBorder(
+				new TitledBorder(null, "Selecionar o Diretorio", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel.setBounds(10, 90, 320, 47);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		txtInserirDiretrio = new JTextField();
 		txtInserirDiretrio.setForeground(Color.LIGHT_GRAY);
 		txtInserirDiretrio.setBounds(6, 17, 221, 20);
 		panel.add(txtInserirDiretrio);
 		txtInserirDiretrio.setText("C:\\logs");
 		txtInserirDiretrio.setColumns(10);
-		
+
 		JButton btnLocalizar = new JButton("Localizar");
+		btnLocalizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				JFileChooser file = new JFileChooser();
+				file.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+				int i = file.showSaveDialog(null);
+				if (i == 1) {
+					// JtextFieldLocal.setText("");
+				} else {
+
+					File arquivo = file.getSelectedFile();
+					String caminho = arquivo.toString();
+					// File[] qtdArquivos = arquivo.listFiles();
+
+					// int cont = 0;
+					List<String> ListaArquivo = new ArrayList<String>();
+
+					for (File f : arquivo.listFiles()) {
+						if (f.isFile()) {
+
+							if (f.getName().endsWith(".tar.gz")) {
+
+								ListaArquivo.add(f.getName().toString());
+							}
+						}
+
+					}
+					ServicoDescompactador aux = new ServicoDescompactador();
+					aux.extrairLogs(caminho, ListaArquivo);
+			
+					 // MetodoDescompactar(arquivo.getAbsolutePath(),ListaArquivo);
+				}
+			}
+		});
 		btnLocalizar.setBounds(237, 16, 73, 23);
-		
 		panel.add(btnLocalizar);
 	}
 }
