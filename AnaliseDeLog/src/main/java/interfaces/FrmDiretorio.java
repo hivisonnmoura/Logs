9c03e8d4b55ec378cf84cb323b0895a08809fd58
matheuspin <matheus.silvestri@gmail.com>
2016-11-07 10:53:45 -0200

    Add files via upload
    Este commit tem como função realizar a interação entre a tela inicial com o sistema de descompactação de arquivos tar.gz

--- AnaliseDeLog/src/main/java/interfaces/FrmDiretorio.java
+++ AnaliseDeLog/src/main/java/interfaces/FrmDiretorio.java
@@ -1,8 +1,13 @@
 package interfaces;
 
 import java.awt.BorderLayout;
+import javax.swing.JFileChooser;
 import java.awt.EventQueue;
 import java.awt.Frame;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,7 +25,12 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.Box;
 import javax.swing.JSeparator;
 import javax.swing.border.TitledBorder;
+
+import filtros.FiltroTarGz;
+
 import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FrmDiretorio extends JFrame {
 
@@ -50,26 +60,58 @@ public class FrmDiretorio extends JFrame {
 	public FrmDiretorio() {
 		setTitle("Logz");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(500,300, 350, 300);
+		setBounds(500, 300, 350, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Selecionar o Diretório", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBorder(
+				new TitledBorder(null, "Selecionar o Diretï¿½rio", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel.setBounds(10, 90, 320, 47);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		txtInserirDiretrio = new JTextField();
 		txtInserirDiretrio.setForeground(Color.LIGHT_GRAY);
 		txtInserirDiretrio.setBounds(6, 17, 221, 20);
 		panel.add(txtInserirDiretrio);
 		txtInserirDiretrio.setText("C:\\logs");
 		txtInserirDiretrio.setColumns(10);
-		
+
 		JButton btnLocalizar = new JButton("Localizar");
+		btnLocalizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				JFileChooser file = new JFileChooser();
+				file.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+				int i = file.showSaveDialog(null);
+				if (i == 1) {
+					// JtextFieldLocal.setText("");
+				} else {
+
+					File arquivo = file.getSelectedFile();
+
+					// File[] qtdArquivos = arquivo.listFiles();
+
+					// int cont = 0;
+					List<String> ListaArquivo = new ArrayList<String>();
+
+					for (File f : arquivo.listFiles()) {
+						if (f.isFile()) {
+
+							if (f.getName().endsWith(".tar.gz")) {
+
+								ListaArquivo.add(f.getName().toString());
+							}
+						}
+
+					}
+					// MetodoDescompactar(arquivo.getAbsolutePath(),ListaArquivo);
+				}
+			}
+		});
 		btnLocalizar.setBounds(237, 16, 73, 23);
 		panel.add(btnLocalizar);
 	}
