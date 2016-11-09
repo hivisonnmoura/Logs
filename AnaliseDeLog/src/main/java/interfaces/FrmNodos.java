c0a27c84f6508a298647f4c7316cff22c620df53
Hivison do Nascimento Moura <hivison.moura@gmail.com>
2016-11-09 09:15:26 -0200

    Atualização do projeto

--- AnaliseDeLog/src/main/java/interfaces/FrmNodos.java
+++ AnaliseDeLog/src/main/java/interfaces/FrmNodos.java
@@ -14,6 +14,9 @@ import javax.swing.JButton;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.BevelBorder;
 import javax.swing.table.DefaultTableModel;
+
+import objetodevalor.OVNoProcesso;
+
 import javax.swing.border.LineBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -24,10 +27,8 @@ public class FrmNodos extends JFrame {
 	private JTable tableNodosCriticos;
 	private JTextField textField;
 	private JTextField textField_1;
+	private DadoTableModel tableModel;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -41,78 +42,65 @@ public class FrmNodos extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public FrmNodos() {
 		setTitle("Logz - Resumo de pontos cr\u00EDticos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 512, 397);
+		setBounds(100, 100, 460, 369);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panelPlanilha = new JPanel();
 		panelPlanilha.setLayout(null);
-		panelPlanilha.setBorder(new TitledBorder(null, "Relatório dos pontos críticos", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelPlanilha.setBorder(new TitledBorder(null, "Relatório dos pontos críticos", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
 		panelPlanilha.setBounds(60, 129, 394, 181);
 		contentPane.add(panelPlanilha);
-		
+
 		tableNodosCriticos = new JTable();
+		tableNodosCriticos.setModel(new DadoTableModel(OVNoProcesso.criarCom(servicoFachada.getNodos())));
 		tableNodosCriticos.setBorder(new LineBorder(Color.LIGHT_GRAY));
 		tableNodosCriticos.setColumnSelectionAllowed(true);
-		tableNodosCriticos.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-				{null, null, null, null, null},
-			},
-			new String[] {
-				"", "New column", "New column", "New column", "New column"
-			}
-		));
 		tableNodosCriticos.setCellSelectionEnabled(true);
 		tableNodosCriticos.setBounds(10, 37, 374, 112);
 		panelPlanilha.add(tableNodosCriticos);
 		
+
 		JButton btnPrximo = new JButton("Pr\u00F3ximo");
 		btnPrximo.setBounds(365, 318, 89, 23);
 		contentPane.add(btnPrximo);
-		
+
 		JComboBox comboBox = new JComboBox();
 		comboBox.setMaximumRowCount(3);
 		comboBox.setBackground(Color.WHITE);
 		comboBox.setBounds(221, 4, 94, 20);
 		contentPane.add(comboBox);
-		
+
 		JLabel lblSelecioneOHorrio = new JLabel("Seleciona a data:");
 		lblSelecioneOHorrio.setBounds(109, 6, 206, 14);
 		contentPane.add(lblSelecioneOHorrio);
-		
+
 		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Filtro de horário", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBorder(
+				new TitledBorder(null, "Filtro de horário", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel.setBounds(142, 34, 231, 82);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblHoraInicial = new JLabel("Hora inicial: ");
 		lblHoraInicial.setBounds(6, 23, 89, 16);
 		panel.add(lblHoraInicial);
-		
+
 		JLabel lblHoraFinal = new JLabel("Hora final: ");
 		lblHoraFinal.setBounds(6, 56, 89, 16);
 		panel.add(lblHoraFinal);
-		
+
 		textField = new JTextField();
 		textField.setBounds(95, 18, 130, 26);
 		panel.add(textField);
 		textField.setColumns(10);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setBounds(95, 50, 130, 26);
 		panel.add(textField_1);
